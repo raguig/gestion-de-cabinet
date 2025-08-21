89000bf39eabb001bfb1db585a5cc37c94ccbbb7
unknown <raguigmohammedamine@gmail.com>
2025-08-21 21:43:06 +0100

    nutridoc final ver3

--- medic-view-dashboard-main/vite.config.ts
+++ medic-view-dashboard-main/vite.config.ts
@@ -6,8 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: true,
-    port: 3000,
+    host: 'https://projet-amine-front.vercel.app',
+    port: 8080,
   },
   plugins: [
     react(),
