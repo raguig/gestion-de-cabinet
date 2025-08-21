f31b798e4d78012ebb4c05aee909269a77d93dbd
unknown <raguigmohammedamine@gmail.com>
2025-08-21 23:50:58 +0100

    nutridoc final ver31

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,4 @@
 import serverless from "serverless-http";
 import app from "../src/server.js";
 
-// Wrap express app into a serverless function
-export const handler = serverless(app);
+export default serverless(app);
