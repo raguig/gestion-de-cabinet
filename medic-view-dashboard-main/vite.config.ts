9065c07ec6e5e87cd78a9c8ebbfa734de186d2ea
unknown <raguigmohammedamine@gmail.com>
2025-08-21 21:40:21 +0100

    nutridoc final ver2

--- medic-view-dashboard-main/vite.config.ts
+++ medic-view-dashboard-main/vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "localhost",
+    host: true,
     port: 3000,
   },
   plugins: [
